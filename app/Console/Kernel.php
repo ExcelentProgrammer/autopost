8a96feb88857a3e2c793b55bd55b5d2f7ad9438d
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 11:05:46 +0500

    write comments

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,27 +1,22 @@
 <?php
 
+// Namespace declaration for the App\Console class
 namespace App\Console;
 
+// Importing Illuminate\Console\Scheduling\Schedule and Illuminate\Foundation\Console\Kernel
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+// Defining the Kernel class which extends ConsoleKernel
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
+    // Define the application's command schedule
     protected function schedule(Schedule $schedule): void
     {
+        // Commented out code for a sample command that runs the 'inspire' command every hour
         // $schedule->command('inspire')->hourly();
     }
 
-    /**
-     * Register the commands for the application.
-     */
+    // Register the commands for the application
     protected function commands(): void
-    {
-        $this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
-    }
-}
