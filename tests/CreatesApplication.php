fa10ee5f5874ef02666cc7e24ce72e3ce5bbd697
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 11:05:54 +0500

    write comments

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -1,21 +1,26 @@
 <?php
 
+// Namespace declaration for the trait
 namespace Tests;
 
+// Import necessary classes and interfaces
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Application;
 
+// Defining the CreatesApplication trait
 trait CreatesApplication
 {
-    /**
-     * Creates the application.
-     */
+    // Method to create the application instance
     public function createApplication(): Application
     {
+        // Include the bootstrap/app.php file and store the returned application instance in the $app variable
         $app = require __DIR__.'/../bootstrap/app.php';
 
+        // Bootstrap the application by calling the bootstrap method on the Kernel instance
         $app->make(Kernel::class)->bootstrap();
 
+        // Return the application instance
         return $app;
     }
 }
+
