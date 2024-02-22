e26b35de61fd0d977093660e898395ea0269445a
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 11:05:53 +0500

    write comments

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,24 +1,31 @@
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
+// Import the axios HTTP library, which allows us to easily issue requests
+// to our Laravel back-end. This library automatically handles sending the
+// CSRF token as a header based on the value of the "XSRF" token cookie.
 import axios from 'axios';
+
+// Make the axios library available in the window object for global use
 window.axios = axios;
 
+// Set the 'X-Requested-With' header to 'XMLHttpRequest' for all axios requests
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
+
+/*
+Commented out code: Echo exposes an expressive API for subscribing to channels
+and listening for events that are broadcast by Laravel. Echo and event
+broadcasting allows your team to easily build robust real-time web applications.
+*/
 
 // import Echo from 'laravel-echo';
 
 // import Pusher from 'pusher-js';
 // window.Pusher = Pusher;
+
+/*
+Uncomment the following lines and replace the placeholders with the actual
+values of your Pusher configuration when you are ready to set up real-time
+functionality with Laravel Echo and Pusher.
+*/
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
@@ -27,6 +34,4 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
 //     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
 //     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
+//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') ===
