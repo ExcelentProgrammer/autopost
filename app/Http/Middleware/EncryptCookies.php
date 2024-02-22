49265f1c527e67e6b546467fbba4579d2a590201
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 11:05:46 +0500

    write comments

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -1,17 +1,17 @@
 <?php
 
+// Define the namespace for the class
 namespace App\Http\Middleware;
 
+// Import the EncryptCookies middleware class from Illuminate\Cookie\Middleware
 use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
 
+// Define the EncryptCookies class, which extends the EncryptCookies middleware class
 class EncryptCookies extends Middleware
 {
-    /**
-     * The names of the cookies that should not be encrypted.
-     *
-     * @var array<int, string>
-     */
+    // Define a protected property $except, which is an array of cookie names that should not be encrypted
     protected $except = [
         //
     ];
 }
+
