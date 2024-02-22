851bc5784676d2e080f5bb37121621b5e3776b14
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 11:05:48 +0500

    write comments

--- app/Http/Middleware/TrimStrings.php
+++ app/Http/Middleware/TrimStrings.php
@@ -1,19 +1,20 @@
 <?php
 
+// Define the namespace for the class
 namespace App\Http\Middleware;
 
+// Import the TrimStrings middleware class from Illuminate\Foundation\Http\Middleware
 use Illuminate\Foundation\Http\Middleware\TrimStrings as Middleware;
 
+// Define the TrimStrings class that extends the Middleware class
 class TrimStrings extends Middleware
 {
-    /**
-     * The names of the attributes that should not be trimmed.
-     *
-     * @var array<int, string>
-     */
+    // Define a protected property $except that is an array of strings
     protected $except = [
+        // The following attributes should not be trimmed
         'current_password',
         'password',
         'password_confirmation',
     ];
 }
+
