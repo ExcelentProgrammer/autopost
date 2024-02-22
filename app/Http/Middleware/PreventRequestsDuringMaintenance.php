d3f70e8eedf21562350918f0c7b095666d2a878f
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 11:05:48 +0500

    write comments

--- app/Http/Middleware/PreventRequestsDuringMaintenance.php
+++ app/Http/Middleware/PreventRequestsDuringMaintenance.php
@@ -1,17 +1,17 @@
 <?php
 
+// Namespace declaration for the class
 namespace App\Http\Middleware;
 
+// Importing the PreventRequestsDuringMaintenance middleware class from Illuminate\Foundation\Http\Middleware
 use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance as Middleware;
 
+// Defining the PreventRequestsDuringMaintenance class which extends the Middleware class
 class PreventRequestsDuringMaintenance extends Middleware
 {
-    /**
-     * The URIs that should be reachable while maintenance mode is enabled.
-     *
-     * @var array<int, string>
-     */
+    // Defining a protected property $except which is an array of URIs that should be reachable while maintenance mode is enabled
     protected $except = [
-        //
+        // Empty array to be filled with specific URIs as needed
     ];
 }
+
