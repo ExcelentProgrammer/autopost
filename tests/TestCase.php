7b148da2f0dd59aa0ed8f59ca1ed788d58d49242
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 11:05:52 +0500

    write comments

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,15 @@
 <?php
 
+// Define the namespace for this file
 namespace Tests;
 
+// Import the BaseTestCase class from Illuminate\Foundation\Testing
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+// Define an abstract class named TestCase that extends BaseTestCase
 abstract class TestCase extends BaseTestCase
 {
+    // Include the CreatesApplication trait
     use CreatesApplication;
 }
+
