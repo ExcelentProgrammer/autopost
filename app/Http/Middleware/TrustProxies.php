0f475c683de302c69e2b33343b73984b2b6e9faf
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 11:05:48 +0500

    write comments

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,12 +1,14 @@
 <?php
 
+// Import necessary namespaces
 namespace App\Http\Middleware;
-
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
+// Define the TrustProxies class, which extends the TrustProxies middleware provided by Laravel
 class TrustProxies extends Middleware
 {
+    // Define a protected property to hold the trusted proxies for this application
     /**
      * The trusted proxies for this application.
      *
@@ -14,15 +16,18 @@ class TrustProxies extends Middleware
      */
     protected $proxies;
 
+    // Define a protected property to hold the headers that should be used to detect proxies
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
     protected $headers =
+        // List of headers that can be used to detect proxies
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
         Request::HEADER_X_FORWARDED_AWS_ELB;
 }
+
