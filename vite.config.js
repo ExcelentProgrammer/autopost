71040e92d95e258a1be924e436b1a179ac50cc16
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 11:05:54 +0500

    write comments

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
+// Import the 'defineConfig' function from the 'vite' package
 import { defineConfig } from 'vite';
+
+// Import the 'laravel' function from the 'laravel-vite-plugin' package
 import laravel from 'laravel-vite-plugin';
 
+// Define the configuration object for Vite
 export default defineConfig({
+    // Specify an array of plugins to be used by Vite
     plugins: [
+        // The 'laravel' plugin is used to integrate Laravel with Vite
         laravel({
+            // Specify the entry points for the application's CSS and JavaScript files
             input: ['resources/css/app.css', 'resources/js/app.js'],
+
+            // Enable automatic refreshing of the browser when changes are detected
             refresh: true,
         }),
     ],
 });
+
